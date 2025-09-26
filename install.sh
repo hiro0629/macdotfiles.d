453b0bb5b37c6d2a79bf18706ae917f58293572a
Tomohisa Hirota <hirota.tomohisa@gmail.com>
2025-09-26 19:40:31 +0900

    git ignore setting

--- install.sh
+++ install.sh
@@ -5,5 +5,10 @@ OS_TYPE=$(uname -s)
 
 ln -sf "$DOTFILES_DIR/zsh/zshrc" "$HOME/.zshrc"
 
+# git
 ln -sf "$DOTFILES_DIR/git/gitconfig" "$HOME/.gitconfig"
+if [[ ! -d "$HOME/.config/git" ]]; then
+	mkdir ~/.config/git
+fi
+ln -sf "$DOTFILES_DIR/git/ignore" "$HOME/.config/git/ignore"
 
