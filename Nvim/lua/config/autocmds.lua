d3450adbace1209d9928fae1a18c83164d175eb6
Tomohisa Hirota <hirota.tomohisa@gmail.com>
2025-10-24 17:08:16 +0900

    renamde dir

--- Nvim/lua/config/autocmds.lua
+++
@@ -1,1 +0,0 @@
--- Auto command definition
--- install.sh
+++ install.sh
@@ -4,40 +4,38 @@ DOTFILES_DIR="$HOME/macdotfiles.d"
 OS_TYPE=$(uname -s)
 
 # zsh
-ln -sf "$DOTFILES_DIR/zsh/zsh_profile" "$HOME/.zsh_profile"
-ln -sf "$DOTFILES_DIR/zsh/zshrc" "$HOME/.zshrc"
-ln -sf "$DOTFILES_DIR/zsh/zsh_aliases" "$HOME/.zsh_aliases"
+ln -sf "$DOTFILES_DIR/zsh.d/zsh_profile" "$HOME/.zsh_profile"
+ln -sf "$DOTFILES_DIR/zsh.d/zshrc" "$HOME/.zshrc"
+ln -sf "$DOTFILES_DIR/zsh.d/zsh_aliases" "$HOME/.zsh_aliases"
 
 # git
-ln -sf "$DOTFILES_DIR/git/gitconfig" "$HOME/.gitconfig"
+ln -sf "$DOTFILES_DIR/git.d/gitconfig" "$HOME/.gitconfig"
 if [[ ! -d "$HOME/.config/git" ]]; then
 	mkdir ~/.config/git
 fi
-ln -sf "$DOTFILES_DIR/git/ignore" "$HOME/.config/git/ignore"
+ln -sf "$DOTFILES_DIR/git.d/ignore" "$HOME/.config/git/ignore"
 
 # tmux
-ln -sf "$DOTFILES_DIR/tmux/tmux.conf" "$HOME/.tmux.conf"
+ln -sf "$DOTFILES_DIR/tmux.d/tmux.conf" "$HOME/.tmux.conf"
 
 # wezterm
 mkdir -p ~/.config/wezterm
-ln -sf "$DOTFILES_DIR/wezterm/wezterm.lua" "$HOME/.config/wezterm/wezterm.lua"
-ln -sf "$DOTFILES_DIR/wezterm/keybind.lua" "$HOME/.config/wezterm/keybind.lua"
+ln -sf "$DOTFILES_DIR/wezterm.d/wezterm.lua" "$HOME/.config/wezterm/wezterm.lua"
+ln -sf "$DOTFILES_DIR/wezterm.d/keybind.lua" "$HOME/.config/wezterm/keybind.lua"
 
 # starship
-ln -sf "$DOTFILES_DIR/starship/starship.toml" "$HOME/.config/starship.toml"
+ln -sf "$DOTFILES_DIR/starship.d/starship.toml" "$HOME/.config/starship.toml"
 
 # nvim
 mkdir -p ~/.config/nvim/lua/config/plugins
-ln -sf "$DOTFILES_DIR/nvim/init.lua" "$HOME/.config/nvim/init.lua"
-ln -sf "$DOTFILES_DIR/nvim/lua/keymaps.lua" "$HOME/.config/nvim/lua/keymaps.lua"
-ln -sf "$DOTFILES_DIR/nvim/lua/options.lua" "$HOME/.config/nvim/lua/options.lua"
+ln -sf "$DOTFILES_DIR/nvim.d/init.lua" "$HOME/.config/nvim/init.lua"
+ln -sf "$DOTFILES_DIR/nvim.d/lua/keymaps.lua" "$HOME/.config/nvim/lua/keymaps.lua"
+ln -sf "$DOTFILES_DIR/nvim.d/lua/options.lua" "$HOME/.config/nvim/lua/options.lua"
 
 # vim
-ln -sf "$DOTFILES_DIR/vim/vimrc" "$HOME/.vimrc"
+ln -sf "$DOTFILES_DIR/vim.d/vimrc" "$HOME/.vimrc"
 
 # yazi
 mkdir -p ~/.config/yazi
-ln -sf "$DOTFILES_DIR/yazi/theme.toml" "$HOME/.config/yazi/theme.toml"
-
-
+ln -sf "$DOTFILES_DIR/yazi.d/theme.toml" "$HOME/.config/yazi/theme.toml"
 
